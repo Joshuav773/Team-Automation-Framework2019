0744b54fd85005c70682f480cc2d6c595d1030a8
joshuav773 <joshuav773@gmail.com>
2019-01-12 01:56:33 -0500

    joshuas branch

--- Ebay/src/main/java/ebayUtil/ListItemForSale.java
+++ Ebay/src/main/java/ebayUtil/ListItemForSale.java
@@ -1,15 +1,21 @@
 package ebayUtil;
 
+import base.CommonAPI;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class ListItemForSale {
+public class ListItemForSale extends CommonAPI {
 
     @FindBy(xpath = "//a[@href='https://www.ebay.com/sl/sell']")public static WebElement sellOption;
+    @FindBy(css = "#smac_complete")public static WebElement inputBox;
+    @FindBy(xpath = "//em[contains (text(), 'Clothing, Shoes & Accessories > Men')]")
+    public static WebElement mensAthleticShoes;
 
 
     public void listItemForSale(){
         sellOption.click();
-
+        inputBox.sendKeys("shoes", Keys.ENTER);
+        mensAthleticShoes.click();
     }
 }
---
+++ Ebay/src/test/java/ebayTests/ListItemForSaleTest.java
@@ -0,0 +1,21 @@
+package ebayTests;
+
+import ebayUtil.ListItemForSale;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ListItemForSaleTest extends ListItemForSale {
+
+    ListItemForSale listItemForSale;
+
+    @BeforeMethod
+    public void initElements(){
+        listItemForSale = PageFactory.initElements(driver, ListItemForSale.class);
+    }
+    @Test
+    public void listItemForSaleTest(){
+        listItemForSale();
+    }
+
+}
