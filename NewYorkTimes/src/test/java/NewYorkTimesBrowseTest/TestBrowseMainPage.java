671378c2e8789cc0ae61b2b45d9dd80a0c8751fd
Fahim Ahmed <fahim@Ares.local>
2019-01-18 13:36:33 -0500

    update

---
+++ NewYorkTimes/src/test/java/NewYorkTimesBrowseTest/TestBrowseMainPage.java
@@ -0,0 +1,97 @@
+package NewYorkTimesBrowseTest;
+
+import NewYorkTimesBrowse.BrowseMainPage;
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import reporting.TestLogger;
+
+public class TestBrowseMainPage extends BrowseMainPage {
+
+    BrowseMainPage objBrowseMainPage;
+
+    @BeforeMethod
+    public void initElements(){
+        objBrowseMainPage = PageFactory.initElements(driver, BrowseMainPage.class);
+    }
+
+    @Test
+    public void worldClickTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        worldClick();
+    }
+    @Test
+    public void americasClickTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        americasClick();
+    }
+    @Test
+    public void homeButtonTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homeButton();
+    }
+    @Test
+    public void usClickTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        usClick();
+    }
+    @Test
+    public void articleClickTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        articleClick();
+    }
+    @Test
+    public void sportsClickTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        sportsClick();
+    }
+    @Test
+    public void nflSectionTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        nflSection();
+    }
+    @Test
+    public void techSectionTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        techSection();
+    }
+    @Test
+    public void scienceSectionTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        scienceSection();
+    }
+    @Test
+    public void politicsSectionTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        politicsSection();
+    }
+    @Test
+    public void nySectionTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        nySection();
+    }
+    @Test
+    public void frontPageTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        frontPage();
+    }
+    @Test
+    public void contactButtonTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        contactButton();
+    }
+    @Test
+    public void chatButtonTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        chatButton();
+    }
+    @Test
+    public void backHomeTest(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        backHome();
+    }
+
+}
+
+
