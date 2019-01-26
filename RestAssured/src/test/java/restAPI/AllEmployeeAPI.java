5623ffa784091329bddb1a618f4e94132934d8c2
joshuav773 <joshuav773@gmail.com>
2019-01-25 23:36:31 -0500

    restAPI finished more or less

--- RestAssured/src/test/java/restAPI/AllEmployeeAPI.java
+++ RestAssured/src/test/java/restAPI/AllEmployeeAPI.java
@@ -3,18 +3,28 @@ package restAPI;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
 
 public class AllEmployeeAPI {
+
     @Test
     public void getAllEmployeeResources() {
-        //TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         Response response = given().when().get("http://info.venturepulse.org:8080/service-webapp/api/AllEmployeeResources").then().statusCode(200).extract().response();
         String statusLine = response.getStatusLine();
         int statusCode = response.getStatusCode();
         String body = response.getBody().prettyPrint();
         Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
         Assert.assertEquals(statusCode, 200);
+    }
+    @Test
+    public void badCallAllEmployeeResources() {
+        Response response = null;
+        int statusCode = 0;
+        try {
+            response = given().when().get("http://info.venturepulse.org:8080/service-webapp/api/AllEmployeeResourcesssss");
+            statusCode = response.getStatusCode();
+        } catch (Exception ex) {
+        }
+        Assert.assertEquals(statusCode, 404);
     }
 }
